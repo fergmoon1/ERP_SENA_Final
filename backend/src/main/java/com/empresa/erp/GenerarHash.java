66405e74c9e48960aaf4dac4638fa9959b85a870
fergmoon1 <luis_fgomez1@soy.sena.edu.co>
2025-06-24 13:43:28 -0500

    Cambios por actualizacion jjwt (nueva version)

--- backend/src/main/java/com/empresa/erp/GenerarHash.java
+++ backend/src/main/java/com/empresa/erp/GenerarHash.java
@@ -6,5 +6,6 @@ public class GenerarHash {
     public static void main(String[] args) {
         String hash = new BCryptPasswordEncoder().encode("1234");
         System.out.println(hash);
+        System.out.println(new org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder().matches("1234", "$2a$10$HzUnILP.0ib3.rszmAnhWeKUhPt35HB0.0Z65ouFLcFirtwKFLAOq"));
     }
 }
